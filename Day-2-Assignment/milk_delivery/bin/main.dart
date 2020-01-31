8f0aa19c444893eeefaa1cd27bb6249b0fc01228
Sahil Garg <sahil@Sahils-MacBook-Air.local>
2020-01-31 18:07:22 +0530

    Random Spoiled number added in Milk delivery prolem

--- Day-2-Assignment/milk_delivery/bin/main.dart
+++ Day-2-Assignment/milk_delivery/bin/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 void main(List<String> arguments) {
   int x_classes;    //for classes of milk
   int packetDemand;
@@ -21,6 +22,8 @@ void main(List<String> arguments) {
   print('Thank you, your order of ${packetDemand} milk packets are accepted');
   var new_list = List.from(packetList);
   int index;
+  var random = Random().nextInt((packetDemand/2).floor());
+  var packetDelivered = packetDemand - random;
   while(packetDemand>0){
     index=0;
     for(iterator=1;iterator<x_classes;iterator++){
@@ -37,7 +40,10 @@ void main(List<String> arguments) {
       new_list[index] = 0;
     }
   }
+  print('Available    Shipped      Remaining\n---------------------------------------------');
   for(iterator=0;iterator<x_classes;iterator++){
-    print('${packetList[iterator]}\t${packetList[iterator]-new_list[iterator]}\t${new_list[iterator]}');
+    print(' ${packetList[iterator]}\t\t${packetList[iterator]-new_list[iterator]}\t\t${new_list[iterator]}');
   }
+  print('\n-\n-\n-\nNumber of packets spoiled during delivery :$random');
+  print('Number of milk packets delivered finally : $packetDelivered');
 }

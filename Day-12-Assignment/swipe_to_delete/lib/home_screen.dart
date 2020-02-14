541814f90d3e2d343ca7440ca1e8dab78e7f7472
Sahil Garg <sahil@Sahils-MacBook-Air.local>
2020-02-14 11:58:05 +0530

    Swipe to delete & add to list-done

---
+++ Day-12-Assignment/swipe_to_delete/lib/home_screen.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> {
+  Future<String> createAlertDialog(BuildContext context) {
+    TextEditingController customController = TextEditingController();
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Enter new City'),
+          content: TextField(
+            keyboardType: TextInputType.text,
+            controller: customController,
+          ),
+          actions: <Widget>[
+            MaterialButton(
+              elevation: 5.0,
+              child: Text('Submit'),
+              onPressed: () {
+                String newCity = customController.text.toString();
+                if (newCity.isNotEmpty) {
+                  Navigator.of(context).pop(newCity);
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  List<String> cities;
+  int keyIterator;
+  @override
+  void initState() {
+    cities = [
+      'Bangalore',
+      'Delhi',
+      'Prayagraj',
+      'Goa',
+      'Noida',
+      'Kota',
+      'Jaipur',
+      'Shimla',
+      'Chandigarh',
+      'Mangalore'
+    ];
+    keyIterator = 0;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text('Cities'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          createAlertDialog(context).then((onValue) {
+            setState(() {
+              cities.add(onValue);
+            });
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.indigo,
+      ),
+      body: SafeArea(
+        child: Container(
+          child: ListView.builder(
+            itemCount: cities.length,
+            itemBuilder: (context, index) {
+              return Dismissible(
+                key: Key((keyIterator++).toString()),
+                child: ListTile(
+                  title: Text(cities.elementAt(index)),
+                  trailing: Icon(Icons.location_on),
+                ),
+                background: Container(color: Colors.red),
+                onDismissed: (direction) {
+                  Scaffold.of(context).showSnackBar(SnackBar(
+                      content: Text("Removed ${cities.elementAt(index)}")));
+                  setState(() {
+                    cities.removeAt(index);
+                  });
+                },
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ Day-12-Assignment/swipe_to_delete/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:swipe_to_delete/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
